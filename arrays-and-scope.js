83008b9fd716fb96b1c346c1ac0add2e1e01ad37
Kasper Zegel <kasper@thebulb.nl>
2021-01-15 09:55:18 +0100

    uitwerking_release

--- arrays-and-scope.js
+++ arrays-and-scope.js
@@ -6,18 +6,34 @@ const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 // verwacht antwoord: 6
 // --------------------------
 
+let count = 0;
+
+for(let i = 0; i < grades.length;i++) {
+    if(grades[i] >= 8){
+        count++;
+    }
+}
+console.log(count);
 
 // --------------------------
 // Opdracht 2: Check welke waarde in de array het hoogste is, en log dit in de console
 // verwacht antwoord: 9
 // --------------------------
 
+const highestValue = Math.max.apply(Math, grades);
+console.log(highestValue);
 
 // --------------------------
 // Opdracht 3: Tel alle waardes van de array bij elkaar op en bereken het gemiddelde. Log dit in de console.
 // verwacht antwoord: 6.642857142857143
 // --------------------------
 
+let sumOfValues = 0;
+
+for(let i = 0; i < grades.length; i++) {
+    sumOfValues += grades[i];
+}
+console.log(sumOfValues / grades.length);
 
 
 
